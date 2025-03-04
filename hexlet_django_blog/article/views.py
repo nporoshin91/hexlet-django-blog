287ad65e9ab6cb82c833f6187893439e2b832c78
Mikalai Paroshyn <mikalai.paroshyn@pandadoc.com>
2025-03-04 12:32:24 +0100

    templates; template extending

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from hexlet_django_blog.article.apps import ArticleConfig
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles/index.html', context={"name": ArticleConfig.name})
